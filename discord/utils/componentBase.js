bb5e867e4c9aecdffb3a685c3f0fae43695fb150
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2021-10-12 22:27:43 -0400

    Make componentBase.js

---
+++ discord/utils/componentBase.js
@@ -0,0 +1,20 @@
+const {MessageActionRow, MessageButton} = require("discord.js")
+
+module.exports = class component{
+    constructor(type, data){
+        if(type == "button"){
+            let row = new MessageActionRow()
+            let currentNum = 0
+            for(let button of data){
+                currentNum = currentNum + 1
+                let object = new MessageButton()
+                .setStyle(button.style || "PRIMARY")
+                .setLabel(button.text)
+                .setCustomId(button.text)
+                row.addComponents(object)
+            }
+            
+            return row
+        }
+    }
+}
