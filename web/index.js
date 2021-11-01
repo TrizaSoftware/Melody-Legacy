85e87cfadc7cb7f57d5bf9375f940b5a6aa3f119
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2021-10-31 23:06:22 -0400

    Allow users to join the discord server.

--- web/index.js
+++ web/index.js
@@ -7,6 +7,10 @@ app.use("/invite", (req, res) => {
   res.redirect("https://discord.com/oauth2/authorize?client_id=726143653898354758&permissions=6445862216&scope=bot%20applications.commands")
 })
 
+app.use("/join", (req, res) => {
+  res.redirect("https://discord.gg/JwzQtgEwFE")
+})
+
 app.use((req, res) => {
   res.status(200).json({message: "OK"})
 });
